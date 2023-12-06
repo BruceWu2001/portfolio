daab47fcb7c4ccf40e5b95474d820f799a879df5
BruceWu2001 <zijian.wu.2022@scis.smu.edu.sg>
2023-12-07 00:48:57 +0800

    feat: updated info for education tjc and smu

--- components/pages/home/tabs/education/segments/SMU.jsx
+++ components/pages/home/tabs/education/segments/SMU.jsx
@@ -3,7 +3,6 @@ import { Cert } from "@/components/pages/home/tabs/education/Cert"
 import { certs } from "@/lib/constants/skills"
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area"
 import { Separator } from "@/components/ui/separator"
-import { cn } from "@/lib/utils"
 
 const SMU = () => {
   return (
@@ -22,7 +21,7 @@ const SMU = () => {
         <div className="relative my-4">
         <ScrollArea>
             <div className="flex space-x-4 pb-4">
-            {certs.smu.map((cert) => (
+            {certs.smu.reverse().map((cert) => (
                 <Cert
                 key={cert.name}
                 cert={cert}
--- components/pages/home/tabs/education/segments/TJC.jsx
+++ components/pages/home/tabs/education/segments/TJC.jsx
@@ -1,4 +1,9 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { certs } from "@/lib/constants/skills";
+import { Cert } from "@/components/pages/home/tabs/education/Cert"
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area"
+import { Separator } from "@/components/ui/separator"
+
 
 const TJC = () => {
   return (
@@ -10,9 +15,25 @@ const TJC = () => {
         </Avatar>
         <p className="text-2xl">TJC</p>
     </div>
-    <div>
-        
-    </div>
+    <div className="flex items-center justify-between">
+        </div>
+        <Separator />
+        <div className="relative my-4">
+        <ScrollArea>
+            <div className="flex space-x-4 pb-4">
+            {certs.tjc.reverse().map((cert) => (
+                <Cert
+                key={cert.name}
+                cert={cert}
+                className="w-[250px]"
+                width={250}
+                height={330}
+                />
+            ))}
+            </div>
+            <ScrollBar orientation="horizontal" />
+        </ScrollArea>
+        </div>
     </>
   )
 }
